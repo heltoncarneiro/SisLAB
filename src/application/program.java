2d23c8ad5a775b15da364c460a7cddcfe07f1f06
Helton Carneiro <heltoncarneiro@outlook.com>
2023-06-20 21:14:32 -0300

    Criação do projeto

---
+++ src/application/program.java
@@ -0,0 +1,11 @@
+package application;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HEllo World");
+		System.out.println("Começo do projeto");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author helto
+ *
+ */
+module sisLAB {
+}
